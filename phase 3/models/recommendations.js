a5acf6a69f6d0cdaed08263d4b6fb5dca221f828
rahalyy <azizrahaly2004@gmail.com>
2025-07-18 03:30:57 +0200

    Update recommendations.js

---
+++ phase 3/models/recommendations.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const recommendationSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: String,
+  rating: Number,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  ratings: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      value: Number
+    }
+  ],
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  comments: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      text: String
+    }
+  ]
+});
+
+module.exports = mongoose.model('Recommendation', recommendationSchema);
